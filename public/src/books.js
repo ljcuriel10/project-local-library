2e1e97626b412f7bce9fdb831352098cb768c30f
ljcuriel10 <ljcuriel0729@gmail.com>
2020-12-17 12:05:09 -0800

    Adding finished books.js file

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,26 @@
-function findAuthorById(authors, id) {}
+const findAuthorById= (authors, id) => authors.find(author => author.id === id );
 
-function findBookById(books, id) {}
+const findBookById = (books, id) => books.find(book => book.id === id);
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const borrowedBooks = books.filter(book => book.borrows[0].returned === false);
+  const returnedBooks = books.filter(book => book.borrows[0].returned === true);
+  return [borrowedBooks, returnedBooks];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let borrowers = [];
+  accounts.forEach(account=> {
+    book.borrows.forEach(transaction => {
+      if(transaction.id === account.id){
+        let accountObj = {...account};
+        accountObj.returned = transaction.returned;
+        borrowers.push(accountObj)
+      }
+    })
+  });
+  return borrowers.slice(0,10);
+}
 
 module.exports = {
   findAuthorById,
