8d5ab4e7416dcdabb9e2be79b5c7b19a66050fe9
ljcuriel10 <ljcuriel0729@gmail.com>
2020-12-17 12:10:38 -0800

    finished home.js file

--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,54 @@
-function totalBooksCount(books) {}
+const totalBooksCount = books => books.length;
 
-function totalAccountsCount(accounts) {}
+const totalAccountsCount = accounts => accounts.length;
 
-function booksBorrowedCount(books) {}
+function booksBorrowedCount(books) {
+  const borrowedBooks = books.filter(book => book.borrows[0].returned === false);
+  return borrowedBooks.length;
+}
 
-function getMostCommonGenres(books) {}
+const sorter = arr => arr.sort((countA,countB)=> countB.count-countA.count );
 
-function getMostPopularBooks(books) {}
+function getMostCommonGenres(books) {
+  let popularGenres = books.reduce((acc, book)=> ((acc[acc.findIndex(d => d.name === book.genre)] || acc[acc.push({name: book.genre,count:0})-1]).count++,acc),[]);
+  return sorter(popularGenres).slice(0,5);
+}
 
-function getMostPopularAuthors(books, authors) {}
+function getMostPopularBooks(books) {
+  let popularBooks = books.reduce((acc, book)=> (( acc[acc.push({name: book.title,count:book.borrows.length})]),acc),[]);
+  return sorter(popularBooks).slice(0,5);
+}
+
+function getMostPopularAuthors(books, authors) {
+  let result = [];
+  let filteredBooks = books.reduce((acc,book) => {
+    acc[book.authorId]
+    ? (acc[book.authorId] += book.borrows.length)
+    : (acc[book.authorId] = book.borrows.length);
+    return acc;
+  }, {});
+
+  let filteredAuth = authors.reduce((acc, author) => {
+    acc[author.id] = Object.values(author.name).join(' ')
+    return acc;
+  }, {});
+
+  for (let key in filteredBooks){
+    let bookId = key;
+    let name = '';
+    for(let key in filteredAuth){
+      if(key == bookId){
+        name = `${filteredAuth[key]}`
+      }
+    }
+    const value = filteredBooks[key];
+    let newObj = {};
+    newObj['name'] = name;
+    newObj['count'] = value;
+    result.push(newObj);
+  }
+  return sorter(result).slice(0,5);
+}
 
 module.exports = {
   totalBooksCount,
