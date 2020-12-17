a2001a13eaaf17a0c50b6aa5394933a133df6200
ljcuriel10 <ljcuriel0729@gmail.com>
2020-12-17 11:57:15 -0800

    adding finished accounts.js file

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,33 @@
-function findAccountById(accounts, id) {}
+const findAccountById = (accounts, id) => accounts.find(account => account.id === id);
 
-function sortAccountsByLastName(accounts) {}
+const sortAccountsByLastName = accounts => accounts.sort((account1, account2) => account1.name.last > account2.name.last ? 1 : -1);
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  let sum = 0;
+  const mappedArray = books.map((book) => book.borrows.forEach((obj) => {
+    if (obj.id === account.id){
+      sum++;
+    }
+  }));
+  return sum;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let possessedBooks = [];
+  const booksInPossesion = books.filter(book => {
+    const {id, title, genre, authorId, borrows} = book
+    if(borrows[0].id === account.id && borrows[0].returned === false){
+      return authors.forEach(author=> {
+      if(author.id === authorId){
+        let tempBook = {id, title, genre, author, borrows};
+        console.log(tempBook);
+        possessedBooks.push(tempBook);
+      };
+    });
+  };
+});
+return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
